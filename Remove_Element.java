29f42831612c71be2d5fee33d1f4fcb053a18eec
Osama-Nimer <osamafares23@yahoo.com>
2025-10-17 20:26:06 +0300

    Remove Element

---
+++ Remove_Element.java
@@ -0,0 +1,24 @@
+public class Remove_Element {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == val)
+                count++;
+        }
+        int[] values = new int[nums.length - count];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] == val)
+                continue;
+            else{
+                values[index] = nums[i];
+                index++;
+            }
+        }
+        for (int i = 0; i < values.length; i++) {
+            nums[i] = values[i];
+        }
+        return (values.length);
+    }
+}
