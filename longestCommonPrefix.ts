11a5f5974416a52995654e1d3b5c4d2be64a7820
Osama-Nimer <osamafares23@yahoo.com>
2025-10-20 20:00:26 +0300

    longestCommonPrefix

---
+++ longestCommonPrefix.ts
@@ -0,0 +1,13 @@
+function longestCommonPrefix(strs: string[]): string {
+    if (!strs.length) return "";
+    strs.sort();
+    let first = strs[0], last = strs[strs.length - 1], result = "";
+    for (let i = 0; i < first.length; i++) {
+        if (first[i] === last[i]) {
+            result += first[i];
+        } else {
+            break;
+        }
+    }
+    return result;
+};
