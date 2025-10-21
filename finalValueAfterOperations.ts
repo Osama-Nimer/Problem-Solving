e77a94186890c5da4920744e7ee7db80ad665a1b
Osama-Nimer <osamafares23@yahoo.com>
2025-10-21 21:03:39 +0300

    isValid

--- finalValueAfterOperations.ts
+++ finalValueAfterOperations.ts
@@ -7,3 +7,9 @@ function finalValueAfterOperations(operations: string[]): number {
             sum++;
     return sum;
 };
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+    return args.length
+};
