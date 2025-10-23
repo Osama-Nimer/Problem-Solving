8cc23d9f0fe656b510510e9a16ba3560ebea1eef
Osama-Nimer <osamafares23@yahoo.com>
2025-10-23 21:28:22 +0300

    singleNumber

---
+++ singleNumber.ts
@@ -0,0 +1,15 @@
+function getCount(num: number, nums: number[]): number {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === num) count++;
+    }
+    return count;
+  }
+
+function singleNumber(nums: number[]): number {
+  for (let i = 0; i < nums.length; i++) {
+    let num: number = getCount(nums[i],nums);
+    if (num === 1) return nums[i];
+  }
+  return 0;
+}
